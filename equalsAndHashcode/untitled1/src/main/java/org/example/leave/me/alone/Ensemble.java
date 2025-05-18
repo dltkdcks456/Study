a7353293c06591188aa50cc2fa962d68a9df897e
dltkdcks456 <sangchan0825@gmail.com>
2025-05-18 15:32:36 +0900

    ordinal

---
+++ equalsAndHashcode/untitled1/src/main/java/org/example/leave/me/alone/Ensemble.java
@@ -0,0 +1,15 @@
+package org.example.leave.me.alone;
+
+public enum Ensemble {
+    SOLO(1), DUET(2), TRIO(3), QUARTET(4), QUINTET(5),
+    SEXTET(6), SEPTET(7), OCTET(8), NONET(9), DECTET(10), TRIPLE_QUARTET(12),
+    DUPLICATESOLO(1);
+
+    private final int numberOfmusicians;
+
+    Ensemble(final int numberOfmusicians) {
+        this.numberOfmusicians = numberOfmusicians;
+    }
+
+    public int getNumberOfmusicians() {return ordinal()}
+}
